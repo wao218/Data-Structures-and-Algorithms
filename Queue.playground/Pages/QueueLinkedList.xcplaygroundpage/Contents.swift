470cb940083d6a158419a36694543128d7e9d701
Wesley Osborne <wesleyosborne1@gmail.com>
2021-03-18 17:12:58 -0400

    Add LinkedList, Stack, and Queue

---
+++ Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+//: [Previous](@previous)
+
+import Foundation
+
+public class QueueLinkedList<T>: Queue {
+  private var list = DoublyLinkedList<T>()
+  public init() {}
+  
+  
+  public func enqueue(_ element: T) -> Bool {
+    list.append(element)
+    return true
+  }
+  
+  public func dequeue() -> T? {
+    guard !list.isEmpty, let element = list.first else {
+      return nil
+    }
+    return list.remove(element)
+  }
+  
+  public var peek: T? {
+    list.first?.value
+  }
+  
+  public var isEmpty: Bool {
+    list.isEmpty
+  }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+  public var description: String {
+    String(describing: list)
+  }
+}
+
+var queue = QueueLinkedList<String>()
+queue.enqueue("Ray")
+queue.enqueue("Brian")
+queue.enqueue("Eric")
+queue
+queue.dequeue()
+queue
+queue.peek
+//: [Next](@next)
