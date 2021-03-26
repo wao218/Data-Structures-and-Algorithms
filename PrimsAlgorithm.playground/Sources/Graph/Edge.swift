ba4b8acbf23b33c127167a15331a5fb9d60c8742
Wesley Osborne <wesleyosborne1@gmail.com>
2021-03-26 09:40:28 -0700

    Prims Algorithm

---
+++ PrimsAlgorithm.playground/Sources/Graph/Edge.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public struct Edge<T> {
+  
+  public let source: Vertex<T>
+  public let destination: Vertex<T>
+  public let weight: Double?
+}
+
+extension Edge: Equatable where T: Equatable {}
